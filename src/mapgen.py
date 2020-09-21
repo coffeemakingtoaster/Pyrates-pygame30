ae18a95d8b6250f3e13655bd4f16f9bd96864fbd
Häußermann Ben <bh070@hdm-stuttgart.de>
2020-09-21 16:55:18 +0200

    Updatet Mapgen

--- src/mapgen.py
+++ src/mapgen.py
@@ -7,23 +7,27 @@ map_length = 600
 max_map_size = 4
 map_types = 4
 
-x_coordinates = random.sample(range(1,map_width),island_count)
-y_coordinates = random.sample(range(1,map_length),island_count)
-map_list = []
-
-class Islands:
-    def __init__(self, x,y,size,typus,visited):
-        self.x = x
-        self.y = y
-        self.size = size
-        self.typus = typus
-        self.visited = visited
 
 
-for i in range(island_count):
-    island = Islands(x_coordinates[i],y_coordinates[i],random.randint(1,max_map_size),random.randint(1,map_types),False)
-    map_list.append(island)
+def mapgen():
+    x_coordinates = random.sample(range(1, map_width), island_count)
+    y_coordinates = random.sample(range(1, map_length), island_count)
+    map_list = []
+    for i in range(island_count):
+        island = {
+            "x": x_coordinates[i],
+            "y": y_coordinates[i],
+            "size": random.randint(1, max_map_size),
+            "type": random.randint(1, map_types),
+            "visited": False
+        }
+        map_list.append(island)
 
-json.dumps(map_list)
-print(map_list)
+    print(map_list)
+    mapfile = open("data/savegame/map.json", "w")
+    mapfile.write(json.dumps(map_list))
+    mapfile.close()
+
+mapgen()
+
 
