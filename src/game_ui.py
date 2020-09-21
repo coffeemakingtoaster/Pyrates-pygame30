a195720f9b7b7f5bd8347cbfafa26605d9cac997
coffemakingtoaster <mh340@hdm-stuttgart.de>
2020-09-21 18:50:59 +0200

    comments

--- src/game_ui.py
+++ src/game_ui.py
@@ -2,6 +2,9 @@ import pygame
 import os
 import math
 
+
+#given an image and an angle this returns the rotated image
+#can directly be drawn to screen
 def rotate_image(image,angle):
     orig_rect = image.get_rect()
     rot_image = pygame.transform.rotate(image, angle)
@@ -10,14 +13,17 @@ def rotate_image(image,angle):
     rot_image = rot_image.subsurface(rot_rect).copy()
     return rot_image
 
+
 def main():
     pygame.init()
+    #init size of the window. The background color is never visible
     width, height = (1600, 900)
     background_color = (0, 0, 0)
 
+    #path to resources (images in this case)
     asset_path = os.path.join(os.getcwd(),"data","img")
-    is_turned = False
 
+    # init UI elements and provide them with coordinates
     overlay = pygame.image.load(os.path.join(asset_path,"overlay.png"))
     ship = pygame.image.load(os.path.join(asset_path,"ship.png"))
     screen = pygame.display.set_mode((width, height))
@@ -26,9 +32,9 @@ def main():
     ressource_visual = pygame.Rect(width / 3, height/2, width / 3, height/2)
     ship_movement_UI = pygame.Rect((width / 3) * 2, 0, width / 3, height)
 
+    # fill the screen with said elements
     pygame.display.set_caption("Pirate game")
     screen.fill(background_color)
-
     pygame.draw.rect(screen,(127,4,50),managment_UI)
     pygame.draw.rect(screen, (0,0,0), ship_visual)
     pygame.draw.rect(screen, (161, 83, 27),ressource_visual)
@@ -36,13 +42,19 @@ def main():
     screen.blit(ship, (1250, 600))
     screen.blit(overlay, (0, 0))
 
+    #flip displayes everything for the user to see
     pygame.display.flip()
 
+    #gameloop
     running = True
     while running:
         for event in pygame.event.get():
+            #Quit
             if event.type == pygame.QUIT:
                 running = False
+            #at mouseclick:
+            #- checks if click was in ship movement window and if so rotates the ship image
+            #TODO: add support for more parts of the screen
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x,mouse_y = pygame.mouse.get_pos()
                 if ship_movement_UI.collidepoint(pygame.mouse.get_pos()):
@@ -51,8 +63,6 @@ def main():
                     screen.blit(rotate_image(ship,angle-90), (1250, 600))
                     screen.blit(overlay, (0, 0))
                     pygame.draw.ellipse(screen, (255,0,0), pygame.Rect(mouse_x,mouse_y,10,10))
-                else:
-                    ship.reset_ship()
         pygame.display.flip()
 
 
