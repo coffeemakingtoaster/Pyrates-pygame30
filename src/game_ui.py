31c9c87c68c53a5d001b9cd0367edb9c2e5d3108
coffemakingtoaster <mh340@hdm-stuttgart.de>
2020-09-21 18:43:19 +0200

    added basic ship movement

--- src/game_ui.py
+++ src/game_ui.py
@@ -1,15 +1,25 @@
 import pygame
 import os
+import math
 
+def rotate_image(image,angle):
+    orig_rect = image.get_rect()
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
 
 def main():
     pygame.init()
     width, height = (1600, 900)
     background_color = (0, 0, 0)
 
-    asset_path = os.path.join("data","img")
+    asset_path = os.path.join(os.getcwd(),"data","img")
+    is_turned = False
 
-    overlay = pygame.image.load(os.path.join(os.getcwd(),asset_path,"overlay.png"))
+    overlay = pygame.image.load(os.path.join(asset_path,"overlay.png"))
+    ship = pygame.image.load(os.path.join(asset_path,"ship.png"))
     screen = pygame.display.set_mode((width, height))
     managment_UI = pygame.Rect(0, 0, width / 3, height)
     ship_visual = pygame.Rect(width / 3, 0, width / 3, height/2)
@@ -19,11 +29,11 @@ def main():
     pygame.display.set_caption("Pirate game")
     screen.fill(background_color)
 
-
     pygame.draw.rect(screen,(127,4,50),managment_UI)
     pygame.draw.rect(screen, (0,0,0), ship_visual)
     pygame.draw.rect(screen, (161, 83, 27),ressource_visual)
     pygame.draw.rect(screen, (10,169, 255), ship_movement_UI)
+    screen.blit(ship, (1250, 600))
     screen.blit(overlay, (0, 0))
 
     pygame.display.flip()
@@ -31,9 +41,18 @@ def main():
     running = True
     while running:
         for event in pygame.event.get():
-            print(event)
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x,mouse_y = pygame.mouse.get_pos()
+                if ship_movement_UI.collidepoint(pygame.mouse.get_pos()):
+                    angle = (180 / math.pi) * -math.atan2((mouse_y - 700 ),(mouse_x - 1350))
+                    pygame.draw.rect(screen, (10, 169, 255), ship_movement_UI)
+                    screen.blit(rotate_image(ship,angle-90), (1250, 600))
+                    screen.blit(overlay, (0, 0))
+                    pygame.draw.ellipse(screen, (255,0,0), pygame.Rect(mouse_x,mouse_y,10,10))
+                else:
+                    ship.reset_ship()
         pygame.display.flip()
 
 
