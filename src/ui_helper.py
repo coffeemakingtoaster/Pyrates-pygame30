75c6c660815e4be17afee7ceaee2a0729b060420
coffemakingtoaster <mh340@hdm-stuttgart.de>
2020-09-22 11:08:12 +0200

    merge prep

--- src/ui_helper.py
+++ src/ui_helper.py
@@ -4,25 +4,38 @@ import game_logic
 
 
 def draw_resources(current_savegame):
+    asset_path = os.path.join(os.getcwd(), "data", "img")
+    text_color = (0,0,0)
     ammunition_values,max_ammunition = current_savegame.get_ammunition()
     supplies_values,max_supply = current_savegame.get_supplies()
     ship_HP,max_ship_HP = current_savegame.get_ship_HP()
     gold = current_savegame.get_gold_value()
-    supplies_bar_basis = pygame.Rect(50,50,250,30)
-    supplies_bar_filled = pygame.Rect(50, 50, (supplies_values/max_supply)*250, 30)##
-    ammunition_bar_basis = pygame.Rect(50, 100, 250, 30)
-    ammunition_bar_filled = pygame.Rect(50, 100, (ammunition_values/max_ammunition)*250, 30)##
-    ship_HP_bar_basis = pygame.Rect(50, 150, 250, 30)
-    ship_HP_bar_filled = pygame.Rect(50, 150, (ship_HP/max_ship_HP)*250, 30)##
-    values_text = pygame.font.Font(os.path.join(os.getcwd(), "data", "other", "Carlito-Regular.ttf"), 20)
+    values_text = pygame.font.Font(os.path.join(os.getcwd(), "data", "other", "Carlito-Regular.ttf"), 30)
+    supplies_bar_basis = pygame.Rect(50,100,250,30)
+    supplies_bar_filled = pygame.Rect(50, 100, (supplies_values/max_supply)*250, 30)##
+    ammunition_bar_basis = pygame.Rect(50, 200, 250, 30)
+    ammunition_bar_filled = pygame.Rect(50, 200, (ammunition_values/max_ammunition)*250, 30)##
+    ship_HP_bar_basis = pygame.Rect(50, 300, 250, 30)
+    ship_HP_bar_filled = pygame.Rect(50, 300, (ship_HP/max_ship_HP)*250, 30)##
+    supplies_text_render = values_text.render(str(supplies_values)+"/"+str(max_supply),False,text_color)
+    ammunition_text_render = values_text.render(str(ammunition_values) + "/" + str(max_ammunition), False, text_color)
+    HP_text_render = values_text.render(str(ship_HP) + "/" + str(max_ship_HP), False, text_color)
+    gold_render = values_text.render(str(gold),False,text_color)
     values_surf = pygame.Surface((533,450))
     values_surf.fill((82, 62, 16))
     pygame.draw.rect(values_surf,(107, 86, 28),supplies_bar_basis)
     pygame.draw.rect(values_surf,(255, 0, 25),supplies_bar_filled)
+    values_surf.blit(supplies_text_render,supplies_text_render.get_rect(center=(400,115)))
     pygame.draw.rect(values_surf,(107, 86, 28),ammunition_bar_basis)
     pygame.draw.rect(values_surf,(255, 0, 25),ammunition_bar_filled)
+    values_surf.blit(ammunition_text_render, ammunition_text_render.get_rect(center=(400, 215)))
     pygame.draw.rect(values_surf,(107, 86, 28),ship_HP_bar_basis)
     pygame.draw.rect(values_surf,(255, 0, 25),ship_HP_bar_filled)
+    values_surf.blit(HP_text_render, HP_text_render.get_rect(center=(400, 315)))
+    gold_rect = gold_render.get_rect(center = (533/2,400))
+    gold_icon = pygame.image.load(os.path.join(asset_path, "coin.png"))
+    values_surf.blit(gold_render,gold_rect)
+    values_surf.blit(pygame.transform.scale(gold_icon,(50,50)),((533/2)+(gold_rect.width/2)+20,375))
     return values_surf
 
 def status_update(title,text):
