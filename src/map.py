2ef81d6b3afb5aab322bde0eae81bdd6dc214723
Häußermann Ben <bh070@hdm-stuttgart.de>
2020-09-22 20:02:58 +0200

    safety push

--- src/map.py
+++ src/map.py
@@ -10,10 +10,10 @@ import game_ui
 asset_path = os.path.join(os.getcwd(), "data", "img")
 
 img_grid = [
-    ["small1.png","mid1.png","large1.png"],
-    ["small2.png","mid2.png","large2.png"],
-    ["small3.png","mid3.png","large3.png"],
-    ["small4.png","mid4.png","large4.png"]
+    ["small1.png","mid1.png","large1.jpg"],
+    ["small2.jpg","mid2.jpg","large2.jpg"],
+    ["small3.jpg","mid3.jpg","large3.jpg"],
+    ["small4.jpg","mid4.jpg","large4.jpg"]
 ]
 
 size_dic = {
@@ -29,7 +29,6 @@ def mapdraw(ship_pos_x,ship_pos_y,screen):
     i = 0
     for island in map:
         if ship_pos_x-500 < island.get("x") < ship_pos_x+500 and  ship_pos_y-600 < island.get("y") < ship_pos_y+600:
-            if(island.get("type")==1):
                 current_island = pygame.image.load(os.path.join(asset_path, img_grid[island.get("type")-1][island.get("size")-1]))
                 screen.blit(current_island, (1350 + (island.get("x") - ship_pos_x),(450 - (island.get("y") - ship_pos_y))))
                 island_hitbox = pygame.Rect(1350 + (island.get("x") - ship_pos_x),(450 - (island.get("y") - ship_pos_y)), size_dic.get(island.get("size")), size_dic.get(island.get("size")))
@@ -37,6 +36,13 @@ def mapdraw(ship_pos_x,ship_pos_y,screen):
                     return {"island_id":i,"island_values":island}
         i+=1
     return None
-
+def collisioncheck(ship_pos_x,ship_pos_y):
+    with open('data/savegame/map.json') as json_file:
+        map = json.load(json_file)
+    i = 0
+    for island in map:
+        if island_hitbox.colliderect(1340,440,20,20):
+            return {"island_id":i,"island_values":island}
+    i+=1
 
 
