b1193e186a5fe9365c6347ce95da9791d2c7fe5c
coffemakingtoaster <mh340@hdm-stuttgart.de>
2020-09-25 22:28:41 +0200

    fixed build script

--- src/builder.py
+++ src/builder.py
@@ -3,14 +3,18 @@ import os
 import shutil
 
 
+
 def main():
     PyInstaller.__main__.run(["--onefile",os.path.join(os.getcwd(),"run_game.py")])
     print("finished building...copying additional files to target directory")
     if os.path.isdir(os.path.join(os.getcwd(),"dist","data")):
         print("clearing old junk")
         shutil.rmtree(os.path.join(os.getcwd(),"dist","data"))
-    shutil.copytree(src=os.path.join(os.getcwd(),"data"),dest=os.path.join(os.getcwd(),"dist","data"))
-    print('\033[92m'+"ALL DONE!")
+    shutil.copytree(src=os.path.join(os.getcwd(),"data"),dst=os.path.join(os.getcwd(),"dist","data"))
+    print("Build finished successfully")
 
 if __name__ == "__main__":
-    main()
+    try:
+       main()
+    except:
+        print("CRITICAL ERROR! BUILD FAILED")
